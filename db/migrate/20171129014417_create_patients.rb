6190a4c2bcf20ec83a6f87d1906c437aa32d6714
BasHud <eugesma@gmail.com>
2017-11-29 00:11:16 -0300

    Added Patients and Professionals' models

---
+++ db/migrate/20171129014417_create_patients.rb
@@ -0,0 +1,14 @@
+class CreatePatients < ActiveRecord::Migration[5.1]
+  def change
+    create_table :patients do |t|
+      t.string :first_name
+      t.string :last_name
+      t.integer :dni
+      t.string :address
+      t.string :email
+      t.string :phone
+
+      t.timestamps
+    end
+  end
+end
