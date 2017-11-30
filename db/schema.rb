c3bead5a0f5c5190385281690eff3a6c481c8933
toriak <raulfvidela@gmail.com>
2017-11-30 20:12:47 -0300

    creada la tabla OrderStatus

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130231007) do
+ActiveRecord::Schema.define(version: 20171130231157) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "order_statuses", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "patients", force: :cascade do |t|
     t.string "first_name"
