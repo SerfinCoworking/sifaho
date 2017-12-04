5512597e25e3c542e7dffa4ac927293c0251c58c
Nicolas Cermesoni <ncermesoni@gmail.com>
2017-12-03 21:39:14 -0300

    fix duplicate line

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -1,6 +1,5 @@
 class Medication < ApplicationRecord
   validates :vademecum, presence: true
-  validates :vademecum, presence: true
   validates :expiration_date, presence: true
   validates :date_received, presence:true
 
