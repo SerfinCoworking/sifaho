922ddd2768ff5a5d74235017a95964b03d44d606
eugesma <eugesma@gmail.com>
2018-06-24 17:20:52 -0300

    Not recoverable. Change Title Page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :recoverable, :rememberable,
+  devise :database_authenticatable, :rememberable,
          :trackable, :validatable
 end
