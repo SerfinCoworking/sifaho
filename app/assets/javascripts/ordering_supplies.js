b1bd030e2b1ea35e9a105b2e704df153826669b4
eugesma <eugesma@gmail.com>
2018-11-21 14:52:51 -0300

    Add origin/destinyto movements of a supply

--- app/assets/javascripts/ordering_supplies.js
+++ app/assets/javascripts/ordering_supplies.js
@@ -100,7 +100,7 @@ $(document).on('turbolinks:load', function() {
          var nested_form = _this.parents(".nested-fields");
          nested_form.find(".supply-id").val(ui.item.value);
          nested_form.find(".supply-name").val(ui.item.name);
-         nested_form.find(".unity").val(ui.item.unity)
+         nested_form.find(".unity").val(ui.item.unity);
          nested_form.find('.selectpicker').prop("disabled", false).selectpicker('refresh');
          nested_form.find(".request-quantity").prop('disabled', false);
          nested_form.find(".select-change").trigger('change');
