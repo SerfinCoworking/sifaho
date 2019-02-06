8a5a035926fe5c0db41670075e5b527584374d8d
BasHud <eugesma@gmail.com>
2019-02-06 11:44:48 -0300

    Fix migration

--- db/migrate/20190206143920_migrate_add_patient_type_to_patients.rb
+++ db/migrate/20190206143920_migrate_add_patient_type_to_patients.rb
@@ -3,7 +3,7 @@ class MigrateAddPatientTypeToPatients < ActiveRecord::Migration[5.1]
     Patient.find_each do |patient|
       if patient.patient_type.present?
       else
-        patient.patient_type = 1
+        patient.patient_type = PatientType.find_by_id(1)
         patient.save(:validate => false)
       end
     end
