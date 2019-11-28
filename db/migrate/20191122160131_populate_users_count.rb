804325ec44e1bdf3f2ccbc194231bc952198788c
BasHud <eugesma@gmail.com>
2019-11-28 14:47:23 -0300

    Delete migration

--- db/migrate/20191122160131_populate_users_count.rb
+++
@@ -1,7 +0,0 @@
-class PopulateUsersCount < ActiveRecord::Migration[5.2]
-  def change
-    Establishment.find_each do |establishment|
-      Establishment.reset_counters(establishment.id, :users)
-    end
-  end
-end
