71e1e7058c46d3248f1b6c6cc84d55b9b6b909bf
BasHud <eugesma@gmail.com>
2020-06-01 13:48:55 -0300

    uncomment line create profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true
 
-  # after_create :create_profile # Comment in development
+  after_create :create_profile # Comment in development
 
   # Delegaciones
   delegate :full_name, :dni, :email, to: :profile
@@ -42,7 +42,7 @@ class User < ApplicationRecord
   
   def verify_profile
     unless self.profile.present?
-      # self.create_profile # Comment in development
+      self.create_profile # Comment in development
     end
     unless self.sector.present?
       if self.sectors.present?
