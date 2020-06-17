edd9fd1fa0f28bb7e8dc84c9f67b64579685a8f4
paul <paul.18.sma@gmail.com>
2020-06-17 09:16:58 -0300

    removes complexity_level column from sectors

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_21_134543) do
+ActiveRecord::Schema.define(version: 2020_06_17_121454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
@@ -565,7 +565,6 @@ ActiveRecord::Schema.define(version: 2020_01_21_134543) do
   create_table "sectors", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.integer "complexity_level"
     t.integer "user_sectors_count", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
