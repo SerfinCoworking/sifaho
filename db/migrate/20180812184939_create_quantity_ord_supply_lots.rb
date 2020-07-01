6128c03361671472795b7412cef24a4b22bbd5b8
BasHud <eugesma@gmail.com>
2020-06-30 22:07:22 -0300

    remove supply lot integer from quantity ord supply lots

--- db/migrate/20180812184939_create_quantity_ord_supply_lots.rb
+++ db/migrate/20180812184939_create_quantity_ord_supply_lots.rb
@@ -1,7 +1,6 @@
 class CreateQuantityOrdSupplyLots < ActiveRecord::Migration[5.1]
   def change
     create_table :quantity_ord_supply_lots do |t|
-      t.integer :supply_lot
       t.string :lot_code
       t.references :quantifiable, polymorphic: true, index: { name: 'quant_ord_sup_lot_poly' }
       t.integer :requested_quantity, default: 0
