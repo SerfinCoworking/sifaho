4918e0f6caec0714bce703d2ef8e3cfbe12a7070
paul <paul.18.sma@gmail.com>
2020-07-15 12:05:34 -0300

    improve notifications list

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_06_27_192416) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "fuzzystrmatch"
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
   enable_extension "unaccent"
@@ -117,13 +118,6 @@ ActiveRecord::Schema.define(version: 2020_06_27_192416) do
     t.datetime "updated_at", null: false
     t.index ["bedroom_id"], name: "index_beds_on_bedroom_id"
     t.index ["service_id"], name: "index_beds_on_service_id"
-  end
-
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "cities", force: :cascade do |t|
@@ -449,6 +443,7 @@ ActiveRecord::Schema.define(version: 2020_06_27_192416) do
 
   create_table "products", force: :cascade do |t|
     t.bigint "unity_id"
+    t.bigint "area_id"
     t.string "code"
     t.string "name"
     t.text "description"
@@ -456,6 +451,7 @@ ActiveRecord::Schema.define(version: 2020_06_27_192416) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deleted_at"
+    t.index ["area_id"], name: "index_products_on_area_id"
     t.index ["deleted_at"], name: "index_products_on_deleted_at"
     t.index ["unity_id"], name: "index_products_on_unity_id"
   end
@@ -598,17 +594,12 @@ ActiveRecord::Schema.define(version: 2020_06_27_192416) do
     t.string "name"
     t.string "description"
     t.string "observation"
-    t.string "unity", limit: 100
+    t.string "unity"
     t.boolean "needs_expiration"
-    t.boolean "active_alarm"
-    t.integer "period_alarm"
-    t.integer "expiration_alarm"
-    t.integer "quantity_alarm"
-    t.integer "period_control"
     t.boolean "is_active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "supply_area_id"
+    t.bigint "supply_area_id", default: 38
     t.datetime "deleted_at"
     t.index ["deleted_at"], name: "index_supplies_on_deleted_at"
     t.index ["supply_area_id"], name: "index_supplies_on_supply_area_id"
@@ -685,13 +676,11 @@ ActiveRecord::Schema.define(version: 2020_06_27_192416) do
   add_foreign_key "patient_phones", "patients"
   add_foreign_key "patients", "addresses"
   add_foreign_key "permission_requests", "users"
-  add_foreign_key "products", "unities"
   add_foreign_key "reports", "sectors"
   add_foreign_key "reports", "supplies"
   add_foreign_key "reports", "users"
   add_foreign_key "sectors", "establishments"
   add_foreign_key "states", "countries"
-  add_foreign_key "supplies", "supply_areas"
   add_foreign_key "supply_lots", "laboratories"
   add_foreign_key "supply_lots", "supplies"
   add_foreign_key "users", "sectors"
