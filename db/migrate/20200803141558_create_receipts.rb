717bb66484dea54d07a55a586324221e3c4240da
paul <paul.18.sma@gmail.com>
2020-08-10 15:30:08 -0300

    adds receipt movements

--- db/migrate/20200803141558_create_receipts.rb
+++ db/migrate/20200803141558_create_receipts.rb
@@ -5,8 +5,10 @@ class CreateReceipts < ActiveRecord::Migration[5.2]
       t.references :provider_sector, index: true
       t.references :applicant_sector, index: true
       t.references :created_by, index: true
+      t.references :received_by, index: true
       t.integer :status, default: 0
       t.text :observation
+      t.datetime :received_date
       t.timestamps
     end
   end
