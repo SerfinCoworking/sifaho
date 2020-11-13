a463b8bfc73b6d17d8442a34e0c181baf10e54c1
paul <paul.18.sma@gmail.com>
2020-11-13 12:11:22 -0300

    improve chronic_dispensations and chroni_prescription_products tables

--- db/migrate/20201029120646_create_chronic_dispensations.rb
+++ db/migrate/20201029120646_create_chronic_dispensations.rb
@@ -3,6 +3,7 @@ class CreateChronicDispensations < ActiveRecord::Migration[5.2]
     create_table :chronic_dispensations do |t|
       t.references :chronic_prescription, index: true
       t.text :observation
+      t.integer :status
       t.timestamps
     end
   end
--- db/migrate/20201029120709_create_chronic_prescription_products.rb
+++ db/migrate/20201029120709_create_chronic_prescription_products.rb
@@ -1,10 +1,10 @@
 class CreateChronicPrescriptionProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :chronic_prescription_products do |t|
+      t.references :original_chronic_prescription_product, index: {name: :unique_org_chronic_prescription_product_cpp}
       t.references :chronic_dispensation, index: true
       t.references :product, index: true
       
-      t.integer :request_quantity
       t.integer :delivery_quantity
 
       t.text :observation
