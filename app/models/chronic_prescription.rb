fd08749cede1f681d66d849e9e0b6b1ba7193268
eugesma@gmail.com <eugesma@gmail.com>
2021-02-04 10:14:51 -0300

    fix: notify type

--- app/models/chronic_prescription.rb
+++ app/models/chronic_prescription.rb
@@ -104,7 +104,7 @@ class ChronicPrescription < ApplicationRecord
   def create_notification(of_user, action_type)
     ChronicPrescriptionMovement.create(user: of_user, chronic_prescription: self, action: action_type, sector: of_user.sector)
     (of_user.sector.users.uniq - [of_user]).each do |user|
-      @not = Notification.where( actor: of_user, user: user, target: self, notify_type: "crónica", action_type: action_type, actor_sector: of_user.sector ).first_or_create
+      @not = Notification.where( actor: of_user, user: user, target: self, notify_type: "cronica", action_type: action_type, actor_sector: of_user.sector ).first_or_create
       @not.updated_at = DateTime.now
       @not.read_at = nil
       @not.save
