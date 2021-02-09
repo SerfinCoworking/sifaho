64bcbe3b870674e2836737653d12fcde83b64f03
paul <paul.18.sma@gmail.com>
2021-02-08 23:58:21 -0300

    fix migration

--- db/migrate/20210209011056_add_date_dispensed_to_outpatient_prescriptions.rb
+++ db/migrate/20210209011056_add_date_dispensed_to_outpatient_prescriptions.rb
@@ -5,17 +5,21 @@ class AddDateDispensedToOutpatientPrescriptions < ActiveRecord::Migration[5.2]
     # Buscamos todas las prescripciones ambulatorias del bk con estado "dispensada" y le cargamos el valor de "date_dispensed"
     @old_prescriptions = Prescription.ambulatoria.dispensada.where.not(dispensed_at: nil)
     @old_prescriptions.each do |outpatient_prescription|
-      outpateiton_pres = OutpatientPrescription.where(id: outpatient_prescription.id).first
-      if outpateiton_pres.present?
-        outpateiton_pres.update(date_dispensed: outpatient_prescription.dispensed_at)
-        puts "Old Ambulatoria: #{outpateiton_pres.id} - Dispensada: #{outpateiton_pres.date_dispensed.strftime("%d/%m/%Y")}".colorize(:green)
+      outpatient_pres = OutpatientPrescription.where(id: outpatient_prescription.id).first
+      if outpatient_pres.present?
+        outpatient_pres.date_dispensed = outpatient_prescription.dispensed_at
+        outpatient_pres.save(validate: false)
+
+        puts "Old Ambulatoria: #{outpatient_pres.id} - Dispensada: #{outpatient_pres.date_dispensed.strftime("%d/%m/%Y")}".colorize(:green)
       end
     end
     
     # Buscamos todas las prescripciones ambulatorias post migracion con estado "dispensada" y le cargamos el valor de "updated_at"
     @new_prescriptions = OutpatientPrescription.dispensada.where.not(id: @old_prescriptions.pluck(:id))
     @new_prescriptions.each do |outpatient_prescription|
-      outpatient_prescription.update(date_dispensed: outpatient_prescription.updated_at)
+      outpatient_prescription.date_dispensed = outpatient_prescription.updated_at
+      outpatient_prescription.save(validate: false)
+
       puts "New Ambulatoria: #{outpatient_prescription.id} - Dispensada: #{outpatient_prescription.date_dispensed.strftime("%d/%m/%Y")}".colorize(:green)
     end
 
