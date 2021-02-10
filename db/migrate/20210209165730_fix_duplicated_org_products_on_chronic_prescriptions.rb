7b1506d57874d50da8b099005fb08744eafeed60
paul <paul.18.sma@gmail.com>
2021-02-10 12:48:14 -0300

    remove duplicated original products from chronic_prescriptions

--- db/migrate/20210209165730_fix_duplicated_org_products_on_chronic_prescriptions.rb
+++ db/migrate/20210209165730_fix_duplicated_org_products_on_chronic_prescriptions.rb
@@ -1,40 +1,59 @@
 class FixDuplicatedOrgProductsOnChronicPrescriptions < ActiveRecord::Migration[5.2]
   def up
     # Buscamos las prescripciones que aun estan pendientes o parcialmente dispensadas
-    ChronicPrescription.where(status: [:pendiente, :dispensada_parcial]).limit(50).each do |chronic_pres|
+    ChronicPrescription.where(status: [:pendiente, :dispensada_parcial]).each do |chronic_pres|
 
       # obtenemos los id de los productos
-      all_original_products = chronic_pres.original_chronic_prescription_products.select(:product_id).pluck(:product_id)
+      all_original_products = chronic_pres.original_chronic_prescription_products.pluck(:product_id)
       # filtramos los ids, solo los que se repiten
       reapeted_product_ids = all_original_products.select{ |e| all_original_products.count(e) > 1}
       
       if reapeted_product_ids.count > 0
-       puts chronic_pres.id.to_s
-        # obtenemos el primer ord product de los id repetidos (haciendo un uniq) 
-        # ordenados por request_quantity (nos quedamos con el que mayor cantidad a dispensar tiene)
-        @org_product = chronic_pres.original_chronic_prescription_products.where(product_id: reapeted_product_ids.uniq).order(:request_quantity).first
         
-        @org_product_to_destroy = chronic_pres.original_chronic_prescription_products.where(product_id: reapeted_product_ids.uniq).order(:request_quantity).where.not(id: @org_product.id)
-
-        @org_product_to_destroy.each do |org_prod_to_destroy|
-          # Primero actualizamos la cantidad dispensada en el original product
-          @org_product.total_delivered_quantity += org_prod_to_destroy.total_delivered_quantity
-          @org_product.save(validate: false)
+        puts "Receta a Actualizar: #{chronic_pres.id.to_s}".colorize(:green)
+        # por cada repetido "unico" realizamos el proceso correspondiente
+        reapeted_product_ids.uniq.each do |target_product| 
+          # obtenemos el primer original product ordenados por request_quantityuest_quantity (nos quedamos con el que mayor cantidad a dispensar tiene)
           
-          # Segundo debemos actualizar las relaciones de org_prod_to_destroy
-          org_prod_to_destroy.chronic_prescription_products.each do |cpp|
-            cpp.original_chronic_prescription_product_id = @org_product.id
-            cpp.save(validate: false)
-          end
-          # Tercero, y finalmente eliminamos el registro duplicado
-          puts "Original Chronic Prescription Product eliminado: " + org_prod_to_destroy.id.to_s.colorize(:red)
-          org_prod_to_destroy.destroy
+          @org_product = chronic_pres.original_chronic_prescription_products.where(product_id: target_product).order(:request_quantity).first
+          
+          # Buscamos todos los productos, y excluimos a @org_product
+          @org_product_to_destroy = chronic_pres.original_chronic_prescription_products.where(product_id: target_product).where.not(id: @org_product.id)
+
+          # Por cada original product a eliminar encontrado, vamos a obtener el total_delivered_quantity y sus relaciones
+          @org_product_to_destroy.each do |org_prod_to_destroy|
+            puts "Producto original repetido #{org_prod_to_destroy.id}".colorize(:blue)
+            # Primero actualizamos la cantidad dispensada en el original product
+            org_product_total_delivered_quantity(@org_product, org_prod_to_destroy)
+          
+            # Actualizamos los productos registrados, con el id del produco original que va a quedar.
+            update_chronic_prescription_products(@org_product, org_prod_to_destroy)
+              
+            # Tercero, y finalmente eliminamos el registro duplicado
+            puts "Original Chronic Prescription Product eliminado: " + org_prod_to_destroy.id.to_s.colorize(:red)
+            org_prod_to_destroy.destroy
+          end 
         end
       end
-     
     end
   end
   
   def down
   end
+
+  private
+  def org_product_total_delivered_quantity(org, org_to_del)
+    org.total_delivered_quantity += org_to_del.total_delivered_quantity
+    org.save(validate: false)
+    puts "Cantidad total entregada actualizada: #{org.total_delivered_quantity}".colorize(background: :green)
+  end
+  
+  def update_chronic_prescription_products(org, org_to_del)
+    org_to_del.chronic_prescription_products.each do |cpp|
+      cpp.original_chronic_prescription_product_id = org.id
+      cpp.save(validate: false)
+      puts "Chronic Prescription Product actualizado: #{cpp.id}".colorize(background: :green)
+    end
+  end
+
 end
