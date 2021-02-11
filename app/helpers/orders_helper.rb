194e6ed3154adbe00b613105855f0c7ce9161bcf
Eugenio Gomez <eugesma@gmail.com>
2021-02-11 12:48:06 -0300

    add order to fields

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -4,4 +4,14 @@ module OrdersHelper
       return 'table-info'
     end
   end
+
+  def sort_order_products(object, action)
+    if ["new"].any? { |string| action.include? string }
+      return object.order_products.build
+    elsif ["edit", "update"].any? { |string| action.include? string }
+      return object.order_products.joins(:product).order("products.name")
+    elsif ["create"].any? { |string| action.include? string }
+      return object.order_products
+    end
+  end
 end
