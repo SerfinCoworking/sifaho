702e593c9e48862ab89befbf813a0f5d7fca330b
paul <paul.18.sma@gmail.com>
2021-03-01 09:12:33 -0300

    removes quantity_type from dispensation_types

--- db/migrate/20210223180436_create_dispensation_types.rb
+++ db/migrate/20210223180436_create_dispensation_types.rb
@@ -3,7 +3,6 @@ class CreateDispensationTypes < ActiveRecord::Migration[5.2]
     create_table :dispensation_types do |t|
       t.references :chronic_dispensation, index: true
       t.references :original_chronic_prescription_product, index: {name: :unique_org_chron_pres_on_dispensation_types}
-      t.integer :quantity_type, default: 0
       t.integer :quantity
       t.timestamps
     end
