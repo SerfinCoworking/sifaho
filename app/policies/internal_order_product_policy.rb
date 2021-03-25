255ec1e4acebb91d0f542e151b85a8071e15f9ad
Eugenio Gomez <eugesma@gmail.com>
2021-03-25 10:46:58 -0300

    fix: permit remove products in applicant edition

--- app/policies/internal_order_product_policy.rb
+++ app/policies/internal_order_product_policy.rb
@@ -2,11 +2,23 @@ class InternalOrderProductPolicy < ApplicationPolicy
   def edit_request_quantity?
     if record.is_provision? || (!record.is_provision? && record.new_record?)
       user.has_any_role?(:admin, :farmaceutico, :auxiliar_farmacia, :central_farmaceutico, :medic, :enfermero)
+    elsif !record.is_provision? && record.get_order.solicitud_auditoria?
+      user.has_any_role?(:admin, :farmaceutico, :auxiliar_farmacia, :central_farmaceutico, :medic, :enfermero)
     end
   end
 
   def edit_product?
     if record.is_provision? || (!record.is_provision? && record.new_record?)
+      user.has_any_role?(:admin, :farmaceutico, :auxiliar_farmacia, :central_farmaceutico, :medic, :enfermero)
+    elsif !record.is_provision? && record.get_order.solicitud_auditoria?
+      user.has_any_role?(:admin, :farmaceutico, :auxiliar_farmacia, :central_farmaceutico, :medic, :enfermero)
+    end
+  end
+
+  def remove_association?
+    if record.is_provision? || record.new_record?
+      user.has_any_role?(:admin, :farmaceutico, :auxiliar_farmacia, :central_farmaceutico, :medic, :enfermero)
+    elsif !record.is_provision? && record.get_order.solicitud_auditoria?
       user.has_any_role?(:admin, :farmaceutico, :auxiliar_farmacia, :central_farmaceutico, :medic, :enfermero)
     end
   end
