d3e87bfb01fe99599fc4978e5f4a9adfa616668b
Eugenio Gomez <eugesma@gmail.com>
2021-04-27 14:41:25 -0300

    feat: add requested by and sent by to external order report

--- app/models/external_orders/external_order_movement.rb
+++ app/models/external_orders/external_order_movement.rb
@@ -1,5 +1,15 @@
 class ExternalOrderMovement < ApplicationRecord
+  include PgSearch
+
+  # Relations
   belongs_to :user
   belongs_to :external_order
   belongs_to :sector
+
+  # Scopes
+  scope :by_action, ->(action_string) { where('action LIKE ?', action_string) }
+
+  pg_search_scope :search_action,
+    against: :action,
+    :ignoring => :accents # Ignorar tildes.
 end
