9b418a238ee3d9963e3a3776e5a43a930976c69f
Eugenio Gomez <eugesma@gmail.com>
2021-05-03 11:31:22 -0300

    fix: image, user and patient profile.

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -3,14 +3,23 @@ module ProfileHelper
     if patient.avatar.attached?
       main_app.url_for(patient.avatar.variant(resize: "#{first_size}x#{second_size}^", gravity: "center", crop: "#{first_size}x#{second_size}+0+0"))
     else
-      patient.save
-      main_app.url_for(patient.avatar.variant(resize: "#{first_size}x#{second_size}^", gravity: "center", crop: "#{first_size}x#{second_size}+0+0"))
+      image_path('profile-placeholder.jpg')
     end
   end
 
   def professional_avatar(professional, size=40)
     if professional.avatar.attached?
       main_app.url_for(professional.avatar.variant(resize: "#{size}x#{size}^", gravity: "center",crop: "#{size}x#{size}+0+0"))
+    else
+      image_path('profile-placeholder.jpg')
+    end
+  end
+
+  def user_avatar(user, size=40)
+    if user.profile.avatar.attached?
+      main_app.url_for(user.profile.avatar.variant(resize: "#{size}x#{size}^", gravity: "center",crop: "#{size}x#{size}+0+0"))
+    else
+      image_path('profile-placeholder.jpg')
     end
   end
 end
