6ba5ab4c57c1073f13c1cf74a569fbb697b75e33
Eugenio Gomez <eugesma@gmail.com>
2021-06-10 10:35:49 -0300

    fix: chronic migration

--- db/migrate/20210610101832_migrate_original_chronic_prescription_products.rb
+++ db/migrate/20210610101832_migrate_original_chronic_prescription_products.rb
@@ -2,11 +2,13 @@ class MigrateOriginalChronicPrescriptionProducts < ActiveRecord::Migration[5.2]
   def change
     OriginalChronicPrescriptionProduct.all.find_each do |product|
       unless product.terminado_manual?
-        if product.total_delivered_quantity <= product.total_request_quantity
+        if product.total_request_quantity <= product.total_delivered_quantity
           puts "Original product id: #{product.id}, estado: #{product.treatment_status}"
           product.treatment_status = 'terminado'
-          product.save(validate: false)
+        else
+          product.treatment_status = 'pendiente'
         end
+        product.save(validate: false)
       end
     end
   end
