da3d5c1b1a30c31c1fccec53330437d867be72e0
paul <paul.18.sma@gmail.com>
2021-06-15 12:03:39 -0300

    adds patient info to products assignment

--- app/controllers/inpatient_prescriptions_controller.rb
+++ app/controllers/inpatient_prescriptions_controller.rb
@@ -31,7 +31,7 @@ class InpatientPrescriptionsController < ApplicationController
   def new
     @inpatient_prescription = InpatientPrescription.new
     @inpatient_prescription.parent_order_products.build
-    @inpatients = Patient.all.limit(10)
+    @inpatients = Patient.last(10)
   end
 
   # GET /inpatient_prescriptions/1/edit
