34993313ef61031987d4755db04aca84fb43e78a
Eugenio Gomez <eugesma@gmail.com>
2021-07-05 12:47:46 -0300

    fix: remove accent from notify type

--- app/models/inpatient_prescription.rb
+++ app/models/inpatient_prescription.rb
@@ -93,7 +93,7 @@ class InpatientPrescription < ApplicationRecord
     InpatientPrescriptionMovement.create(user: of_user, order: self, order_product: order_product,
                                          action: action_type, sector: of_user.sector)
     (of_user.sector.users.uniq - [of_user]).each do |user|
-      @not = Notification.where(actor: of_user, user: user, target: self, notify_type: 'internación',
+      @not = Notification.where(actor: of_user, user: user, target: self, notify_type: 'internacion',
                                 action_type: action_type, actor_sector: of_user.sector).first_or_create
       @not.updated_at = DateTime.now
       @not.read_at = nil
