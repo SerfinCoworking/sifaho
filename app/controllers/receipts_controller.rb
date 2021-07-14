40d06090e659e6eed36795299acb2b76b3da9dc9
Eugenio Gomez <eugesma@gmail.com>
2021-07-14 08:46:00 -0300

    add print btn to receipts

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -17,6 +17,12 @@ class ReceiptsController < ApplicationController
   # GET /receipts/1
   # GET /receipts/1.json
   def show
+    respond_to do |format|
+      format.pdf do
+        send_data generate_order_report(@receipt), filename: "recibo_#{@receipt.remit_code}.pdf", type: 'application/pdf',
+                                                   disposition: 'inline'
+      end
+    end
   end
 
   # GET /receipts/new
@@ -94,10 +100,10 @@ class ReceiptsController < ApplicationController
       ensure
         @sectors = @receipt.provider_sector.present? ? @receipt.provider_sector.establishment.sectors : []
         @receipt_products = @receipt.receipt_products.present? ? @receipt.receipt_products : @receipt.receipt_products.build
-        
+
         format.html { render :edit }
         format.json { render json: @receipt.errors, status: :unprocessable_entity }
-      end       
+      end
     end
   end
 
@@ -119,6 +125,71 @@ class ReceiptsController < ApplicationController
     respond_to do |format|
       format.js
     end
+  end
+
+  def generate_order_report(stock)
+    report = Thinreports::Report.new
+
+    report.use_layout File.join(Rails.root, 'app', 'reports', 'stock', 'one_stock_report.tlf'), default: true
+
+    # Comenzamos con la pagina principal
+    report.start_new_page
+
+    # Agregamos el encabezado
+    report.page[:title] = 'Reporte de stock de un producto'
+    report.page[:requested_date] = DateTime.now.strftime('%d/%m/%YY')
+    report.page[:efector] = current_user.sector_name+" "+current_user.establishment_name
+    report.page[:product_code].value(@stock.product_code)
+    report.page[:product_name].value(@stock.product_name)
+    report.page[:product_area].value(@stock.product_area_name)
+    report.page[:stock_quantity].value(@stock.total_quantity)
+    report.page[:username].value("DNI: "+current_user.dni.to_s+", "+current_user.full_name)
+
+    @movements = @stock.movements.sort_by{|e| e[:created_at]}.last(10).reverse
+    @lot_stocks = @stock.lot_stocks.greater_than_zero
+
+    report.page[:movements_title].value("Últimos "+@movements.count.to_s+" movimientos")
+    # Se van agregando los productos
+    @movements.each_with_index do |movement, index|
+      # Luego de que la primer pagina ya halla sido rellenada, agregamos la pagina defualt (no tiene header)      
+      report.list(:movements_list) do |list|
+        list.add_row do |row|
+          row.values line_number: index + 1,
+            date: movement.created_at.strftime("%d/%m/%Y"),
+            lot: movement.lot_stock.lot_code,
+            movement: movement.order.class.model_name.human,
+            origin_name: movement.order_origin_name,
+            destiny_name: movement.order_destiny_name,
+            received_quantity: movement.adds? ? movement.quantity : 0,
+            delivered_quantity: movement.adds? ? 0 : movement.quantity
+        end
+      end
+    end
+    
+    report.page[:lots_title].value(@lot_stocks.count.to_s+" lotes en stock")
+
+    @lot_stocks.each_with_index do |lot_stock, index|
+      # Luego de que la primer pagina ya halla sido rellenada, agregamos la pagina defualt (no tiene header)      
+      report.list(:lot_stocks_list) do |list|
+        list.add_row do |row|
+          row.values line_number: index + 1,
+            lot: lot_stock.lot_code,
+            laboratory: lot_stock.lot_laboratory_name,
+            expiry_date: lot_stock.lot_expiry_date_string,
+            status: lot_stock.lot_status.humanize,
+            quantity: lot_stock.quantity,
+            reserved_quantity: lot_stock.reserved_quantity,
+            total_quantity: lot_stock.total_quantity 
+        end
+      end
+    end
+
+    # A cada pagina le agregamos el pie de pagina
+    report.pages.each do |page|
+      page[:page_count] = report.page_count
+    end
+
+    report.generate
   end
 
   private
