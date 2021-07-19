f40cc4a8ab80bd8b5ccc4bfba40a29cff2f25c6f
paul <paul.18.sma@gmail.com>
2021-07-19 15:05:33 -0300

    fix professionals search

--- app/services/professional_creator.rb
+++ app/services/professional_creator.rb
@@ -3,9 +3,6 @@ class ProfessionalCreator
     @first_name = param[:first_name]
     @last_name = param[:last_name]
     @id = param[:id] || ''
-
-    puts "===================="
-    puts @id
   end
 
   def find_practitioner
@@ -19,22 +16,24 @@ class ProfessionalCreator
                                                     params: { 'given': @first_name, 'family': @last_name }
                                                   }
                                                 )
-    JSON.parse(@practitioners).map do |doc|
+
+    @formated_practitioners = JSON.parse(@practitioners).map do |doc|
       first_name = doc['name'][0]['given'].join(' ')
       last_name = doc['name'][0]['family'].join(' ')
       dni_filtered = doc['identifier'].select { |identifier| identifier['system'] == 'http://www.renaper.gob.ar/dni' }
       dni = dni_filtered[0]['value']
-      sex = doc['gender']
-      enrollments = doc['qualification'].map do |qual|
-        { name: qual['identifier'][0]['value'], code: qual['code']['text'] } if qual['identifier'].present?
+      sex = doc['gender'] || 'indeterminate'
+      enrollments_filtered = doc['qualification'].select { |qual| qual['identifier'].present? && qual['code'].present? }
+      enrollments = []
+      unless enrollments_filtered.empty?
+        enrollments = enrollments_filtered.map do |qual|
+          { name: qual['identifier'][0]['value'], code: qual['code']['text'] }
+        end
       end
-      { id: @id,
-        first_name: first_name,
-        last_name: last_name,
-        fullname: "#{last_name} #{first_name}",
-        dni: dni,
-        sex: sex,
-        qualifications_attributes: enrollments }
+
+      { id: @id, first_name: first_name, last_name: last_name, fullname: "#{last_name} #{first_name}", dni: dni,
+        sex: sex, qualifications_attributes: enrollments }
     end
+    @formated_practitioners.select { |prac| prac[:qualifications_attributes].present? }
   end
 end
