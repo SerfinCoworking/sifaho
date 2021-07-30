b8f934987e8338394007383478130f8e4a0382c6
paul <paul.18.sma@gmail.com>
2021-07-30 11:30:25 -0300

    adds switch button for theme changes

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -1,12 +1,25 @@
 $(document).on('turbolinks:load', function() {
   $('input[name="theme"]').on('change', function(e){
     const isLight = $(e.target).prop('checked');
+    const url = $(e.target).attr('data-url');
+    let value = '';
     if(isLight){
       $("#wrapper").removeClass("light").addClass('dark');
+      value = 'dark';
     }else{
       $("#wrapper").removeClass("dark").addClass("light");
+      value = 'light';
     }
-    console.log($(e.target).prop('checked'), "<==========00");
-    
+    updateTheme(url, value);
   });
+
+  function updateTheme(url, theme){
+    $.ajax({
+      url: url,
+      method: 'PATCH',
+      data: { 
+        profile: { theme } 
+      }
+    });
+  }
 });
