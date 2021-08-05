e43f4fad9286c34e81a78023cc46e391e1690aff
Eugenio Gomez <eugesma@gmail.com>
2021-08-05 11:45:06 -0300

    fix: create lot provenance

--- app/models/effectors/laboratory.rb
+++ app/models/effectors/laboratory.rb
@@ -13,35 +13,35 @@ class Laboratory < ApplicationRecord
       :sorted_by,
       :search_name,
       :search_cuit,
-      :search_gln,
+      :search_gln
     ]
   )
 
   pg_search_scope :search_name,
-  against: :name,
-  :using => { :tsearch => {:prefix => true} }, # Buscar coincidencia desde las primeras letras.
-  :ignoring => :accents # Ignorar tildes.
+                  against: :name,
+                  using: { tsearch: { prefix: true } }, # Buscar coincidencia desde las primeras letras.
+                  ignoring: :accents # Ignorar tildes.
 
   pg_search_scope :search_cuit,
-  against: :cuit,
-  :using => { :tsearch => {:prefix => true} }, # Buscar coincidencia desde las primeras letras.
-  :ignoring => :accents # Ignorar tildes.
+                  against: :cuit,
+                  using: { tsearch: { prefix: true } }, # Buscar coincidencia desde las primeras letras.
+                  ignoring: :accents # Ignorar tildes.
 
   pg_search_scope :search_gln,
-  against: :gln,
-  :using => { :tsearch => {:prefix => true} }, # Buscar coincidencia desde las primeras letras.
-  :ignoring => :accents # Ignorar tildes.
+                  against: :gln,
+                  using: { tsearch: { prefix: true } }, # Buscar coincidencia desde las primeras letras.
+                  gnoring: :accents # Ignorar tildes.
 
   scope :sorted_by, lambda { |sort_option|
     # extract the sort direction from the param value.
-    direction = (sort_option =~ /desc$/) ? 'desc' : 'asc'
+    direction = sort_option =~ /desc$/ ? 'desc' : 'asc'
     case sort_option.to_s
     when /^razon_social_/
       # Ordenamiento por nombre del profesional
-      order("LOWER(laboratories.name) #{ direction }")
+      order("LOWER(laboratories.name) #{direction}")
     else
       # Si no existe la opcion de ordenamiento se levanta la excepcion
-      raise(ArgumentError, "Invalid sort option: #{ sort_option.inspect }")
+      raise(ArgumentError, "Invalid sort option: #{sort_option.inspect}")
     end
   }
 end
