a7ac79de2c663e1a38d12944b7f938997324dd61
paul <paul.18.sma@gmail.com>
2021-08-31 08:51:43 -0300

    removes head comment

--- app/assets/javascripts/order_product_lot_stock/form.js
+++ app/assets/javascripts/order_product_lot_stock/form.js
@@ -9,18 +9,6 @@ $(document).on('turbolinks:load', function(e){
   initProductsEvents();
   
   // button submit
-<<<<<<< HEAD
-/*   $("button[type='submit']").on('click', function(e){
-    e.preventDefault();
-    $(e.target).attr('disabled', true);
-    $(e.target).siblings('button, a').attr('disabled', true);
-    $(e.target).find("div.c-msg").css({"display": "none"});
-    $(e.target).find('div.d-none').toggleClass('d-none');
-    $('input[name="commit"][type="hidden"]').val($(e.target).attr('data-value')).trigger('change');
-    $('form#'+$(e.target).attr('form')).submit();
-  });
- */
-=======
   // $("button[type='submit']").on('click', function(e){
   //   e.preventDefault();
   //   $(e.target).attr('disabled', true);
@@ -31,7 +19,6 @@ $(document).on('turbolinks:load', function(e){
   //   $('form#'+$(e.target).attr('form')).submit();
   // });
 
->>>>>>> 30e3bb9d5c9c0fed866463495b4844c8c122eafd
   // cocoon init
   $('#order-product-cocoon-container').on('cocoon:after-insert', function(e, inserted_item) {
     initProductsEvents();
