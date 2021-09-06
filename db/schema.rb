050f920793160f2b21b8cf9f1d05739154da2f42
paul <paul.18.sma@gmail.com>
2021-09-06 10:57:05 -0300

    adds reserved_quantity to ext_ord_prod_lot_stock and int_ord_prod_lot_stock

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_02_175926) do
+ActiveRecord::Schema.define(version: 2021_09_06_135216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
@@ -300,6 +300,7 @@ ActiveRecord::Schema.define(version: 2021_09_02_175926) do
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "reserved_quantity", default: 0
     t.index ["external_order_product_id"], name: "index_ext_ord_prod_lot_stocks_on_external_order_product_id"
     t.index ["lot_stock_id"], name: "index_ext_ord_prod_lot_stocks_on_lot_stock_id"
   end
@@ -554,6 +555,7 @@ ActiveRecord::Schema.define(version: 2021_09_02_175926) do
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "reserved_quantity", default: 0
     t.index ["internal_order_product_id"], name: "index_int_ord_prod_lot_stocks_on_internal_order_product_id"
     t.index ["lot_stock_id"], name: "index_int_ord_prod_lot_stocks_on_lot_stock_id"
   end
