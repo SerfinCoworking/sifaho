d9dd96b0dc1f83abec93c452d2838687ab9c14ae
Eugenio Gomez <eugesma@gmail.com>
2021-09-17 09:09:22 -0300

    feat: add print to outpatient prescriptions

--- app/services/report_services/outpatient_prescription_report_service.rb
+++ app/services/report_services/outpatient_prescription_report_service.rb
@@ -9,28 +9,27 @@ module ReportServices
     def generate_pdf
       report = Thinreports::Report.new
 
-      report.use_layout File.join(Rails.root, 'app', 'reports', 'external_order', 'other_page.tlf'), default: true
-      report.use_layout File.join(Rails.root, 'app', 'reports', 'external_order', 'first_page.tlf'), id: :cover_page
+      report.use_layout File.join(Rails.root, 'app', 'reports', 'outpatient_prescription', 'other_page.tlf'), default: true
+      report.use_layout File.join(Rails.root, 'app', 'reports', 'outpatient_prescription', 'first_page.tlf'), id: :cover_page
 
-      # Comenzamos con la pagina principal
+      # Start with the main layout
       report.start_new_page layout: :cover_page
 
-      # Agregamos el encabezado
-      report.page[:title] = 'Pedido de establecimiento'
+      # Add the header
+      report.page[:title] = 'Receta ambulatoria'
       report.page[:remit_code] = @prescription.remit_code
-      report.page[:requested_date] = @prescription.requested_date.strftime('%d/%m/%YY')
+      report.page[:prescribed_date] = @prescription.date_prescribed.strftime('%d/%m/%Y')
+      report.page[:expiry_date] = @prescription.expiry_date.strftime('%d/%m/%Y')
       report.page[:patient_fullname] = @prescription.patient_fullname.titleize
       report.page[:patient_dni] = @prescription.patient_dni
       report.page[:patient_age] = @prescription.patient_age_string
-      report.page[:provider_efector] = @prescription.provider_sector.sector_and_establishment
-      report.page[:provider_user] = @prescription.sent_provision_by_user_fullname
+      report.page[:professional_fullname] = @prescription.professional_fullname
+      report.page[:professional_qualifications] = @prescription.professional_qualifications.map { |pq| ["#{pq.name} #{pq.code}"] }.join(', ')
       report.page[:observations] = @prescription.observation
-      report.page[:products_count].value(@prescription.order_products.count)
-      report.page[:observations_count].value("solicitante "+@prescription.order_products.where.not(applicant_observation: [nil, ""]).count.to_s+" / proveedor "+@prescription.order_products.where.not(provider_observation: [nil, ""]).count.to_s)
+      report.page[:user_info].value("DNI: #{@user.dni}, #{@user.full_name}")
 
-      # Se van agregando los productos
-      @prescription.order_products.joins(:product).order("name").each do |eop|  
-        # Luego de que la primer pagina ya halla sido rellenada, agregamos la pagina defualt (no tiene header)
+      # Add the products
+      @prescription.outpatient_prescription_products.joins(:product).order('name').each do |eop|
         report.start_new_page if report.page_count == 1 && report.list.overflow?
 
         report.list do |list|
@@ -44,8 +43,7 @@ module ReportServices
                   row.values  product_code: eop.product.code,
                     product_name: eop.product.name,
                     requested_quantity: eop.request_quantity.to_s+" "+eop.product.unity.name.pluralize(eop.request_quantity),
-                    obs_req: eop.applicant_observation,
-                    obs_del: eop.provider_observation
+                    observation: eop.observation
 
                   row.item(:border).show if eop.order_prod_lot_stocks.count == 1
                 end
