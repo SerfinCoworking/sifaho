65bdfa065b59c32d460e2249568bc774f174788c
Eugenio Gomez <eugesma@gmail.com>
2021-09-21 14:29:45 -0300

    fix: outpatient and chronic prescriptions list

--- app/controllers/prescriptions/prescriptions_controller.rb
+++ app/controllers/prescriptions/prescriptions_controller.rb
@@ -20,6 +20,9 @@ class PrescriptionsController < ApplicationController
     @chronic_prescriptions = ChronicPrescription.where(patient_id: params[:patient_id]).order(updated_at: :desc).limit(10)
     @outpatient_prescriptions = OutpatientPrescription.where(patient_id: params[:patient_id]).order(updated_at: :desc).limit(10)
     @last_prescription = (@chronic_prescriptions + @outpatient_prescriptions).sort_by(&:updated_at).last
+    respond_to do |format|
+      format.js
+    end
   end
 
   # GET /prescriptions/1
